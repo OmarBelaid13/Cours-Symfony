53f2c8b70d04240a7efee5da263289f3b6724deb
OmarBelaid13 <o.belaid@gmail.com>
2022-05-13 14:58:07 +0200

    Découverte de la notion de container

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Taxes\Calculator;
+use Cocur\Slugify\Slugify;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class HelloController {
 
+    protected $calculator;
+
+    public function __construct(Calculator $calculator)
+    {
+        $this->calculator = $calculator;
+    }
+    
     /**
      * @Route("/hello/{prenom?World}", name="hello", methods={"GET", "POST"}, host="localhost", schemes={"http", "https"})
      */
-    public function hello(string $prenom)
+    public function hello(string $prenom, Slugify $slug, Environment $twig)
     {
+        dump($twig);
+        dump($slug->slugify("Hello World"));
+
+        $tva = $this->calculator->calcul(100);
+        dump($tva);
+
         return new Response("Hello $prenom !");
     }
 }
